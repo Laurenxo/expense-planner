b297359c3671d69f1cab24921e1671e809298b6c
Man Nguyen <nguyenvanman1505@gmail.com>
2021-11-14 15:53:20 +0700

    Initial project

---
+++ expense_planner/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter App'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Container(
+                child: const Text('Chart'),
+              ),
+              elevation: 5,
+              color: Colors.blue,
+            ),
+          ),
+          const Card(
+            color: Colors.red,
+            child: Text('List of tx'),
+          ),
+        ],
+      ),
+    );
+  }
+}
